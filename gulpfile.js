5f97182771e7ab45769b93909bd29b3cd31a0a93
Sofia <sofikononova1@gmail.com>
2025-09-16 23:13:58 +0300

    лабораторна робота

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cssnano = require("gulp-cssnano");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const browserSync = require("browser-sync").create();
+
+// HTML → dist/
+const htmlTask = () => {
+  return src("app/index.html")
+    .pipe(dest("dist"))
+    .pipe(browserSync.stream());
+};
+
+// SCSS → dist/css/style.min.css
+const scssTask = () => {
+  return src("app/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
+};
+
+// JS → dist/js/main.min.js
+const jsTask = () => {
+  return src("app/js/*.js")
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("dist/js"))
+    .pipe(browserSync.stream());
+};
+
+// IMG → dist/imgs/
+const imgTask = () => {
+  return src("app/img/**/*")
+    .pipe(imagemin())
+    .pipe(dest("dist/imgs"))
+    .pipe(browserSync.stream());
+};
+
+// Сервер + watcher
+const serve = () => {
+  browserSync.init({
+    server: { baseDir: "dist" }
+  });
+  watch("app/index.html", htmlTask);
+  watch("app/scss/*.scss", scssTask);
+  watch("app/js/*.js", jsTask);
+  watch("app/img/**/*", imgTask);
+};
+
+// Збірка
+exports.default = series(
+  parallel(htmlTask, scssTask, jsTask, imgTask),
+  serve
+);
