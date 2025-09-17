b1665eeeb53dab4e7a657ddd0ad854a748bdec50
Sofia <sofikononova1@gmail.com>
2025-09-17 12:03:06 +0300

    lab

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.createElement("button");
+  btn.textContent = "Натисни мене";
+  document.body.appendChild(btn);
+
+  btn.addEventListener("click", () => {
+    alert("Кнопка працює!");
+  });
+});
