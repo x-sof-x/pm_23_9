68377890cd6af5579654020ce2355024dba6675b
Sofia <sofikononova1@gmail.com>
2025-10-28 12:04:36 +0200

    Тепер розтягується ліва колонка за правою, фон додається після фото

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 1️⃣ Підставляємо ім'я
     const fullName = `
         <div class="first">JHON</div>
         <div class="last">ABIRAR</div>
@@ -8,18 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
     if (nameElement) {
         nameElement.innerHTML = fullName;
     }
-
-    // 2️⃣ Аккордеон для CONTACT INFO
     const contactArrow = document.querySelector(".contact-info .arrow");
     if (contactArrow) {
         contactArrow.addEventListener("click", () => {
             const content = contactArrow.closest(".contact-info").querySelector(".block-content");
-            content.classList.toggle("active");  // показ/приховування всього рядка
-            contactArrow.classList.toggle("rotate");  // обертання стрілки
+            content.classList.toggle("active");  
+            contactArrow.classList.toggle("rotate"); 
         });
     }
-
-    // 3️⃣ Динамічний блок досвіду
     const experienceData = [
     {
         title: "Enter Job Position Here",
@@ -33,15 +28,17 @@ document.addEventListener("DOMContentLoaded", () => {
         date: "2017 - 2019",
         description: "Lorem Ipsum dolor sit amet, this is a thema consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
     },
-   
+   {
+        title: "Enter Job Position Here",
+        company: "Company Name / Location",
+        date: "2017 - 2019",
+        description: "Lorem Ipsum dolor sit amet, this is a thema consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+    },
 ];
-
-
     function renderExperience(containerId, data) {
         const container = document.getElementById(containerId);
         if (!container) return;
         container.innerHTML = "";
-
         data.forEach(item => {
             const jobItem = document.createElement("div");
             jobItem.classList.add("job-item");
@@ -58,10 +55,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     ${item.description}
                 </div>
             `;
-
             container.appendChild(jobItem);
         });
     }
-
     renderExperience("experience-container", experienceData);
 });
